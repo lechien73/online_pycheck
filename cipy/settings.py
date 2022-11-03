31c97ad34cb888751b6977544a1748ab5a8a66f3
Matt Rudge <matt.rudge@codeinstitute.net>
2022-11-03 16:50:08 +0000

    Change host

--- cipy/settings.py
+++ cipy/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["cipy.herokuapp.com", "localhost"]
+ALLOWED_HOSTS = ["pep8ish.herokuapp.com", "localhost"]
 
 
 # Application definition
